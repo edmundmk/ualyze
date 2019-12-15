0c3a09241be52afcc28532f9bd1a8d538b599920
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2019-12-15 18:12:05 +0000

    make UTF-16 decode clearer.

--- source/ual_paragraph.cpp
+++ source/ual_paragraph.cpp
@@ -36,28 +36,40 @@ bool ual_next_paragraph( ual_buffer* ub, ual_paragraph* out_paragraph )
     }
 
     // Perform analysis.
-    size_t i = 0;
-    int prev = UCDN_LINEBREAK_CLASS_XX;
+    size_t i = ub->p.lower;
+    unsigned prev = UCDN_LINEBREAK_CLASS_XX;
     while ( i < ub->s.size() )
     {
         // Decode character from UTF-16.
         size_t inext = i;
         char32_t uc = ub->s[ inext++ ];
-        bool is_hi_surrogate = ( uc & 0xFC00 ) == 0xD800;
-        bool is_lo_surrogate = ( uc & 0xFC00 ) == 0xDC00;
-        if ( is_hi_surrogate || is_lo_surrogate )
+
+        // Check for surrogate.
+        if ( ( uc & 0xF800 ) == 0xD800 )
         {
-            if ( is_hi_surrogate && inext < ub->s.size() && ( ub->s[ inext ] & 0xFC00 ) == 0xDC00 )
-                uc = 0x010000 + ( ( uc & 0x3FF ) << 10 ) + ( ub->s[ inext++ ] & 0x3FF );
+            // Get next code unit.
+            char32_t ul = inext < ub->s.size() ? ub->s[ inext++ ] : 0;
+
+            // Check for high/low surrogate pair.
+            bool have_hi_surrogate = ( uc & 0xFC00 ) == 0xD800;
+            bool have_lo_surrogate = ( ul & 0xFC00 ) == 0xDC00;
+            if ( have_hi_surrogate && have_lo_surrogate )
+            {
+                // Decode surrogate pair.
+                uc = 0x010000 + ( ( uc & 0x3FF ) << 10 ) + ( ul & 0x3FF );
+            }
             else
+            {
+                // Treat lone surrogates as U+FFFD REPLACEMENT CHARACTER.
                 uc = 0xFFFD;
+            }
         }
 
         // Look up character in ucdn.
-        uint16_t ix = ucdn_get_record_index( uc );
+        unsigned ix = ucdn_get_record_index( uc );
 
         // Check for line break.
-        int curr = ucdn[ ix ].linebreak_class;
+        unsigned curr = ucdn[ ix ].linebreak_class;
         if ( prev == UCDN_LINEBREAK_CLASS_BK
             || prev == UCDN_LINEBREAK_CLASS_NL
             || prev == UCDN_LINEBREAK_CLASS_LF
@@ -67,7 +79,7 @@ bool ual_next_paragraph( ual_buffer* ub, ual_paragraph* out_paragraph )
         }
 
         // No line break, add character to char buffer.
-        ub->c.push_back( { ix, 0 } );
+        ub->c.push_back( { (uint16_t)ix, 0 } );
         if ( uc >= 0x010000 )
         {
             ub->c.push_back( { IX_INVALID, 0 } );
